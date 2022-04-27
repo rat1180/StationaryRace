62add61a43c77b7c191425a1a3136278808a3089
sfslime <93108188+sfslime@users.noreply.github.com>
2022-04-27 11:01:54 +0900

    a

--- StationaryRace/Assets/Car.cs
+++ StationaryRace/Assets/Car.cs
@@ -7,11 +7,9 @@ public class Car : MonoBehaviour
     public float upspeed = 10.0f;   // 前進スピード
     public float backspeed = -5.0f; // 後退スピードorブレーキ
     public float maxspeed = 150;    // 最大加速
-    public float accel = 1.005f;    // 加速値
-    public float decel = 0.995f;    // 減速値
-
-    public bool Accelflg = false;
-
+    public float accel = 1.010f;    // 加速値
+    private float sensitivity = 0.3f;   // 感度
+    public bool Accelflg = false; 
 
     // Start is called before the first frame update
     void Start()
@@ -45,44 +43,56 @@ public class Car : MonoBehaviour
     private void CarMove()
     {
 
-        float x = Input.GetAxisRaw("Horizontal"); // 左 / 右
-        float z = Input.GetAxisRaw("Vertical");   // 上 / 下
-
-        Vector3 direction = new Vector3(0, 0, z).normalized;
-
-        if (Input.GetKey(KeyCode.W)){    // 上矢印キーを押している間
+        if (Input.GetKey(KeyCode.W)) // Wキーを押している間
+        {   
 
             Accelflg = true;
 
             if (Accelflg == true)
             {
-                GetComponent<Rigidbody>().velocity = direction * upspeed;   // 前進のスピード
+                transform.position += transform.forward * Time.deltaTime * upspeed;
 
                 if (upspeed < maxspeed) // upspeedがmaxspeedより小さい間
                 {
                     upspeed *= accel;   // 徐々に加速する
                 }
             }
-            else
+            if (Input.GetKey(KeyCode.A))
             {
-                upspeed *= decel;
+                transform.Rotate(0, -sensitivity, 0);
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                transform.Rotate(0, sensitivity, 0);
             }
         }
-        else if (Input.GetKey(KeyCode.S)) // 下矢印キーを押している間
+        else if(Input.GetKey(KeyCode.S)) // Sキーを押している間
         {
             Accelflg = false;
-
+            
             if (Accelflg == false)
             {
-
-                GetComponent<Rigidbody>().velocity = direction * backspeed; // 後退スピード
+                transform.position -= transform.forward * Time.deltaTime * backspeed;
 
                 if (backspeed < maxspeed) // backspeedがmaxspeedより小さい間
                 {
                     backspeed *= accel;   // accelの値を掛け続ける
                 }
             }
-
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Rotate(0, sensitivity, 0);
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                transform.Rotate(0, -sensitivity, 0);
+            }
         }
+        else
+        {
+            upspeed = 1;
+            backspeed = 1;
+        }
+  
     }
 }

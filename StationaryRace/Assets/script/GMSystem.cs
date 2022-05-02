fda3046023d0a7ebdec7c893cc7d465ab8a7535c
rat1180 <r03.gm.2110112@da.iwasaki.ac.jp>
2022-05-02 15:20:55 +0900

    a

---
+++ StationaryRace/Assets/script/GMSystem.cs
@@ -0,0 +1,124 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ITEMConst;
+
+public class GMSystem : MonoBehaviour
+{
+    /**************
+     ゲーム進行変数
+    ***************/
+
+    //0:停止状態 1:スタート前 2:レース中 3:レース後 
+    private int GameFlg;
+
+    //ランク配列(今は一つ)
+    private int[] Rank;
+
+    /**************
+     ユーザー系変数
+    ***************/
+
+    //1ユーザーに割り当てる情報
+    public struct USERINF
+    {
+        public GameObject USER;
+        public int USERNm;
+        public double CPTime;
+        public int CPcnt;
+        public int Rap;
+    }
+
+    //ユーザー配列（通信形態によっては変更）
+    //USERINF[] Users;
+    private USERINF User;
+
+    //ユーザー人数（通信形態によっては変更）
+    private int Players;
+
+    /***************
+     アイテム系変数
+    ****************/
+
+    //アイテムマネージャー
+    private GameObject ItemManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitSet();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /**************
+     起動時処理
+    ***************/
+
+    //ユーザーなどのセット
+    public void InitSet()
+    {
+        //レース前のシーンからもらう
+        Players = 1;
+
+        //スタート前準備
+        GameFlg = 1;
+
+        //ランク用変数の生成
+        Rank = new int[Players];
+        for(int i = 0; i < Players; i++)
+        {
+            Rank[i] = i;
+        }
+
+        //ユーザー(人数分繰り返す)
+        User.USER = transform.Find("User").gameObject;
+        User.USERNm = 0;
+        User.CPcnt = 0;
+        User.CPTime = 0;
+        User.Rap = 1;
+        NmSend();
+        User.USER.GetComponent<UserOperation>().RankSet();
+
+
+
+        //アイテム
+        //ItemManager = transform.Find("ItemManager").gameObject;
+    }
+
+    //レーススタート
+
+    /***********
+     ユーザー系
+    ************/
+
+    //ユーザー番号を渡す
+    private void NmSend()
+    {
+        int Er = User.USER.GetComponent<UserOperation>().InitUser(User.USERNm);
+        if(Er != 0)
+        {
+            GameFlg = 0;
+        }
+    }
+
+    //ユーザー番号に応じた順位を返す
+    public int RankGet(int Nm)
+    {
+        for (int i = 0; i < Players; i++)
+        {
+            if (Rank[i] == Nm)
+            {
+                return i + 1;
+            }
+        }
+
+        //エラー
+        return -1;
+    }
+
+}

582260459120542e1393a0f4c507821fbb04c76a
siitakee <r03.gm.2110016@da.iwasaki.ac.jp>
2022-05-02 11:29:38 +0900

    a

--- StationaryRace/Assets/script/player_car.cs
+++ StationaryRace/Assets/script/player_car.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class player_car : MonoBehaviour
 {
-    public float speed = 10.0f;        //動く速度
+    public float upspeed = 10.0f;   // 前進スピード
+    public float backspeed = -5.0f; // 後退スピードorブレーキ
+    public float maxspeed = 150;    // 最大加速
+    public float accel = 1.010f;    // 加速値
+    private float sensitivity = 0.3f;   // 感度
+    public bool Accelflg = false;
+
     public int type = 0;               //テクスチャの選択
     public Material[] _material;       //テクスチャの割り当て
 
@@ -17,19 +23,7 @@ public class player_car : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        //前方向に移動
-        if (Input.GetKey(KeyCode.W))
-            this.transform.Translate(0.0f, 0.0f, 0.03f);
-        //左方向に移動
-        if (Input.GetKey(KeyCode.A))
-            this.transform.Translate(-0.03f, 0.0f, 0.0f);
-        //後方向に移動
-        if (Input.GetKey(KeyCode.S))
-            this.transform.Translate(0.0f, 0.0f, -0.03f);
-        //右方向に移動
-        if (Input.GetKey(KeyCode.D))
-            this.transform.Translate(0.03f, 0.0f, 0.0f);
+        CarMove();
 
         //機体のテクスチャ変更
         switch (type)
@@ -44,5 +38,78 @@ public class player_car : MonoBehaviour
                 this.GetComponent<Renderer>().material = _material[2];
                 break;
         }
+    }
+
+    /*
+    public struct Moveflg  // フラグ用構造体
+    {
+        // アクセル用フラグ  true / ON  false / OFF
+        public bool Accelflg;
+
+        // バックorブレーキ用フラグ  true / ON  false / OFF
+        public bool Backflg;
+
+        // アイテム用フラグ  true / ON  false / OFF
+        public bool Itemflg;
+
+        // ハンドル用フラグ  0 : なし  1 : 右  2 : 左
+        public bool Handleflg;
+    } 
+    */
+
+    private void CarMove()
+    {
+
+        if (Input.GetKey(KeyCode.W)) // Wキーを押している間
+        {
+
+            Accelflg = true;
+
+            if (Accelflg == true)
+            {
+                transform.position += transform.forward * Time.deltaTime * upspeed;
+
+                if (upspeed < maxspeed) // upspeedがmaxspeedより小さい間
+                {
+                    upspeed *= accel;   // 徐々に加速する
+                }
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Rotate(0, -sensitivity, 0);
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                transform.Rotate(0, sensitivity, 0);
+            }
+        }
+        else if (Input.GetKey(KeyCode.S)) // Sキーを押している間
+        {
+            Accelflg = false;
+
+            if (Accelflg == false)
+            {
+                transform.position -= transform.forward * Time.deltaTime * backspeed;
+
+                if (backspeed < maxspeed) // backspeedがmaxspeedより小さい間
+                {
+                    backspeed *= accel;   // accelの値を掛け続ける
+                }
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Rotate(0, sensitivity, 0);
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                transform.Rotate(0, -sensitivity, 0);
+            }
+        }
+        else
+        {
+            upspeed = 1;
+            backspeed = 1;
+        }
+
     }
 }

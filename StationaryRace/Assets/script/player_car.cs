09db48c0231b858d72993ccd1f69218e331d3022
siitakee <r03.gm.2110016@da.iwasaki.ac.jp>
2022-05-09 15:25:33 +0900

    a

--- StationaryRace/Assets/script/player_car.cs
+++ StationaryRace/Assets/script/player_car.cs
@@ -4,16 +4,7 @@ using UnityEngine;
 
 public class player_car : MonoBehaviour
 {
-    public float upspeed = 1.0f;   // 前進スピード
-    public float backspeed = -5.0f; // 後退スピードorブレーキ
-    public float maxspeed = 150;    // 最大加速
-    public float accel = 1.010f;    // 加速値
-    private float sensitivity = 0.3f;   // 感度
-    public bool Accelflg = false;
-
-    public int type = 0;               //テクスチャの選択
-    public Material[] _material;       //テクスチャの割り当て
-
+    public int joutai = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,79 +14,16 @@ public class player_car : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        CarMove();
+        
     }
-    /*
-    public void kitai()
+
+    public void State()
     {
-        //機体のテクスチャ変更
-        switch (type)
-        {
-            case 0:
-                this.GetComponent<Renderer>().material = _material[0];
+        switch(joutai){
+            case 0:       //通常
                 break;
-            case 1:
-                this.GetComponent<Renderer>().material = _material[1];
-                break;
-            case 2:
-                this.GetComponent<Renderer>().material = _material[2];
+            case -1:      //被弾時
                 break;
         }
-    }*/
-
-    private void CarMove()
-    {
-
-        if (Input.GetKey(KeyCode.W)) // Wキーを押している間
-        {
-
-            Accelflg = true;
-
-            if (Accelflg == true)
-            {
-                transform.position += transform.forward * Time.deltaTime * upspeed;
-
-                if (upspeed < maxspeed) // upspeedがmaxspeedより小さい間
-                {
-                    upspeed *= accel;   // 徐々に加速する
-                }
-            }
-            if (Input.GetKey(KeyCode.A))
-            {
-                transform.Rotate(0, -sensitivity, 0);
-            }
-            else if (Input.GetKey(KeyCode.D))
-            {
-                transform.Rotate(0, sensitivity, 0);
-            }
-        }
-        else if (Input.GetKey(KeyCode.S)) // Sキーを押している間
-        {
-            Accelflg = false;
-
-            if (Accelflg == false)
-            {
-                transform.position -= transform.forward * Time.deltaTime * backspeed;
-
-                if (backspeed < maxspeed) // backspeedがmaxspeedより小さい間
-                {
-                    backspeed *= accel;   // accelの値を掛け続ける
-                }
-            }
-            if (Input.GetKey(KeyCode.A))
-            {
-                transform.Rotate(0, sensitivity, 0);
-            }
-            else if (Input.GetKey(KeyCode.D))
-            {
-                transform.Rotate(0, -sensitivity, 0);
-            }
-        }
-        else
-        {
-            upspeed = 1;
-            backspeed = 1;
-        }
-
     }
 }

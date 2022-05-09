acb621ede97bc955973da63344ac59b0bee08b1e
monaka826 <r03.gm.2110305@da.iwasaki.ac.jp>
2022-05-09 13:12:40 +0900

    a

---
+++ StationaryRace/Assets/script/UI.cs
@@ -0,0 +1,157 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //テキストを扱う場合は、using UnityEngine.UI; を忘れずに記述
+
+public class UI : MonoBehaviour
+{
+    // 値を受け取るための変数
+    public static int rank;                        
+    public static int draw_item;
+
+    // 順位表示のオブジェクト
+    public GameObject RankNunber1;                
+    public GameObject RankNunber2;                 
+    public GameObject RankNunber3;                　
+    public GameObject RankNunber4;                 
+    public GameObject RankNunber5;                 
+    public GameObject RankNunber6;            
+    
+    // アイテム表示のオブジェクト
+    public GameObject Item_nunber1;
+    public GameObject Item_nunber2;
+    public GameObject Item_nunber3;
+    public GameObject Item_nunber4;
+    public GameObject Item_nunber5;
+    public GameObject Item_nunber6;
+    public GameObject Item_nunber7;
+    public GameObject Item_nunber8;
+    public GameObject Item_nunber9;
+    public GameObject Item_nunber10;
+    //public Text ScoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DRAW_SCORE();
+        DRAW_ITEM();
+    }
+    public void RankingChange(int ranker) // 他スクリプトから値を受け取る 
+    {
+        rank = ranker;
+    }
+    // 値によって表示するオブジェクトを変える
+    void DRAW_SCORE()
+    {
+        RankNunber1.SetActive(false);
+        RankNunber2.SetActive(false);
+        RankNunber3.SetActive(false);
+        RankNunber4.SetActive(false);
+        RankNunber5.SetActive(false);
+        RankNunber6.SetActive(false);
+        switch (rank)
+        {
+            case 1:
+                RankNunber1.SetActive(true);
+                break;
+            case 2:
+                RankNunber2.SetActive(true);
+                break;
+            case 3:
+                RankNunber3.SetActive(true);
+                break;
+            case 4:
+                RankNunber4.SetActive(true);
+                break;
+            case 5:
+                RankNunber5.SetActive(true);
+                break;
+            case 6:
+                RankNunber6.SetActive(true);
+                break;
+            case -1:
+                RankNunber1.SetActive(false);
+                RankNunber2.SetActive(false);
+                RankNunber3.SetActive(false);
+                RankNunber4.SetActive(false);
+                RankNunber5.SetActive(false);
+                RankNunber6.SetActive(false);
+                break;
+
+
+        }
+
+
+    }
+    // 上の処理と同じ
+    public void ITEM_CHANGE(int item)
+    {
+        draw_item = item;
+    }
+    void DRAW_ITEM()
+    {
+        Item_nunber1.SetActive(false);
+        Item_nunber2.SetActive(false);
+        Item_nunber3.SetActive(false);
+        Item_nunber4.SetActive(false);
+        Item_nunber5.SetActive(false);
+        Item_nunber6.SetActive(false);
+        Item_nunber7.SetActive(false);
+        Item_nunber8.SetActive(false);
+        Item_nunber9.SetActive(false);
+        Item_nunber10.SetActive(false);
+
+        switch (draw_item)
+        {
+            case 1:
+                Item_nunber1.SetActive(true);
+                break;
+            case 2:
+                Item_nunber2.SetActive(true);
+                break;
+            case 3:
+                Item_nunber3.SetActive(true);
+                break;
+            case 4:
+                Item_nunber4.SetActive(true);
+                break;
+            case 5:
+                Item_nunber5.SetActive(true);
+                break;
+            case 6:
+                Item_nunber6.SetActive(true);
+                break;
+            case 7:
+                Item_nunber7.SetActive(true);
+                break;
+            case 8:
+                Item_nunber8.SetActive(true);
+                break;
+            case 9:
+                Item_nunber9.SetActive(true);
+                break;
+            case 10:
+                Item_nunber10.SetActive(true);
+                break;
+            case -1:
+                Item_nunber1.SetActive(false);
+                Item_nunber2.SetActive(false);
+                Item_nunber3.SetActive(false);
+                Item_nunber4.SetActive(false);
+                Item_nunber5.SetActive(false);
+                Item_nunber6.SetActive(false);
+                Item_nunber7.SetActive(false);
+                Item_nunber8.SetActive(false);
+                Item_nunber9.SetActive(false);
+                Item_nunber10.SetActive(false);
+                break;
+
+
+        }
+    }
+}

e89605acb35b61effa1744692cf8c7e88d7c1d8e
rat1180 <r03.gm.2110112@da.iwasaki.ac.jp>
2022-05-09 13:31:16 +0900

    a

--- StationaryRace/Assets/title_sozai/firstserect.cs
+++ StationaryRace/Assets/title_sozai/firstserect.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-public class firstserect : MonoBehaviour
+public class FirstSerect : MonoBehaviour
 {
     Button button;
     // Start is called before the first frame update

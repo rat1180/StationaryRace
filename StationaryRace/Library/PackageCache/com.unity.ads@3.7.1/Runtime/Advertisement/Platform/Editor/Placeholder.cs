174da06444403092976549466f36fbd7b39a6c67
rat1180 <r03.gm.2110112@da.iwasaki.ac.jp>
2022-05-11 10:27:55 +0900

    a

--- StationaryRace/Library/PackageCache/com.unity.ads@3.7.1/Runtime/Advertisement/Platform/Editor/Placeholder.cs
+++ StationaryRace/Library/PackageCache/com.unity.ads@3.7.1/Runtime/Advertisement/Platform/Editor/Placeholder.cs
@@ -45,12 +45,12 @@ namespace UnityEngine.Advertisements
             //Only update orientation if we are showing
             if (Screen.width > Screen.height)
             {
-                if (m_CurrentScreenOrientation != ScreenOrientation.LandscapeLeft)
+                if (m_CurrentScreenOrientation != ScreenOrientation.Landscape)
                 {
-                    SwapCanvas(ScreenOrientation.LandscapeLeft);
+                    SwapCanvas(ScreenOrientation.Landscape);
                 }
 
-                m_CurrentScreenOrientation = ScreenOrientation.LandscapeLeft;
+                m_CurrentScreenOrientation = ScreenOrientation.Landscape;
             }
             else
             {
@@ -81,7 +81,7 @@ namespace UnityEngine.Advertisements
                 HideSkipButton(m_PortraitCanvas);
             }
 
-            if (m_CurrentScreenOrientation == ScreenOrientation.LandscapeLeft)
+            if (m_CurrentScreenOrientation == ScreenOrientation.Landscape)
             {
                 m_LandscapeCanvas.SetActive(true);
             }
@@ -186,7 +186,7 @@ namespace UnityEngine.Advertisements
 
         private void SwapCanvas(ScreenOrientation newOrientation)
         {
-            if (newOrientation == ScreenOrientation.LandscapeLeft)
+            if (newOrientation == ScreenOrientation.Landscape)
             {
                 m_LandscapeCanvas.SetActive(true);
                 m_PortraitCanvas.SetActive(false);

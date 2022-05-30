333c95f815b544482ce797adbf0dd751e7223214
rat1180 <r03.gm.2110112@da.iwasaki.ac.jp>
2022-05-30 14:26:47 +0900

    a

---
+++ StationaryRace/Assets/script/ItemScript/BLACKBOARD_ERASER.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BLACKBOARD_ERASER : MonoBehaviour
+{
+    public int durability;//‘Ï‹v’l
+    // Start is called before the first frame update
+    void Start()
+    {
+        durability = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ StationaryRace/Assets/script/ItemScript/MAGIC_PEN.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MAGIC_PEN : MonoBehaviour
+{
+    private int durability;
+    public Vector3 Pos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        durability = 1;
+        Pos = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (durability == 0)//耐久値が0になったら
+        {
+            Destroy(this.gameObject);
+        }
+        Pos.y += 5;
+        Pos.z += 5;
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        // 衝突した相手にPlayerタグが付いているとき.
+        if (collision.gameObject.tag == "Player")
+        {
+            durability -= 1;
+        }
+    }
+}

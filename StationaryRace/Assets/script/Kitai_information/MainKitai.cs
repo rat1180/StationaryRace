8883ecc8d5231856a3dcbc37faacb6bc3bdf676a
siitakee <r03.gm.2110016@da.iwasaki.ac.jp>
2022-06-13 15:34:27 +0900

    a

--- StationaryRace/Assets/script/Kitai_information/MainKitai.cs
+++ StationaryRace/Assets/script/Kitai_information/MainKitai.cs
@@ -1,13 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainKitai : MonoBehaviour
 {
-    private int o_max = 2;             //最大機体番号
-    private float min_y = -13.0f;
-
-    public int Player_Mode = 0;        //機体の状態
+    public int Player_Mode = 10;        //機体の状態
+    public GameObject Skin;            //オブジェクトの割り当て
 
     private const int CharacterChenge =  0;    //キャラ選択
     private const int StartRace       = 10;    //レース前
@@ -17,60 +16,49 @@ public class MainKitai : MonoBehaviour
     private const int DownStage       = 50;    //ステージから落ちた
     private const int ORIGAMI_CRANE   = 60;    //鶴の折り紙に変身
 
-    public int MachineNum = 0;         //機体番号
-    public GameObject Skin;          //オブジェクトの割り当て
-    private int SkinNm;
+    private int MachineNum = 0;         //機体番号
+    private int SkinNum = 0;
+
+    public CountTime countTime;
+    private float Atime = 10;
 
     // Start is called before the first frame update
     void Start()
     {
-        Skin = this.transform.Find("Skin").gameObject;
 
-        for (int i = 0; i < Skin.transform.childCount; i++)
-        {
-            Skin.transform.GetChild(i).gameObject.SetActive(false);
-        }
-
-        Skin.transform.GetChild(SkinNm).gameObject.SetActive(true);
-
-        //skin = GameObject.Find("Car01");
-        //skinChild = new Trensform[skin.transform.childCount];
-
-        //o_max = this.transform.childCount;        //子オブジェクトの個数を取得
-        //kitaiObject = new GameObject[o_max];      //インスタンス作成
-
-        //全ての子オブジェクトを取得
-        //for (int i = 0; i < o_max; i++)
-        //{
-        //    kitaiObject[i] = transform.GetChild(i).gameObject;
-        //}
-
-        //全ての子オブジェクトを非アクティブ
-        //foreach (GameObject gamObj in kitaiObject)
-        //{
-        //    gamObj.SetActive(false);
-        //}
-        //一つだけアクティブにする
-        //kitaiObject[MachineNum].SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
+        PLmode();
+    }
+
+    private void PLmode()
+    {
+        Atime -= Time.deltaTime;
+
         switch (Player_Mode)
         {
-            //キャラ選択
-            case 0:
-                MachineNumber();
-                break;
+            ////キャラ選択
+            //case 0:
+            //    KeyProcess();
+            //    break;
             //スタート前
             case 10:
+                MachineMode();
                 break;
             //レース中
             case 20:
+                if(Atime <= 0)
+                {
+                    Player_Mode = 30;
+                }
                 break;
             //ゴール
             case 30:
+                Debug.Log("ゴール");
+                SceneManager.LoadScene("KitaiSelect");
                 break;
             case 40:
                 break;
@@ -84,71 +72,17 @@ public class MainKitai : MonoBehaviour
     }
 
     //機体の状態の受け取り
-    public void MachineMode()
+    private void MachineMode()
     {
-        //if (Player_Mode == 10)
+        //if (Player_Mode == 0)
         //{
         //    MachineNumber();
         //}
-    }
 
-    public void MachineNumber()
-    {
-        //MachineNum = machine_num;
-
-        if (Input.GetKeyDown("q"))
+        if (countTime.gamestart == 1)
         {
-            ////現在のアクティブな子オブジェクトを非アクティブ
-            //kitaiObject[MachineNum].SetActive(false);
-            //MachineNum++;
-            Skin.transform.GetChild(MachineNum).gameObject.SetActive(false);
-            MachineNum++;
-
-            //子オブジェクトをすべて切り替えたらまた最初のオブジェクトに戻る
-            if (MachineNum == Skin.transform.childCount) { MachineNum = 0; }
-
-            KitaiChange();   //機体変更
-
-        }
-
-        if (Input.GetKeyDown(KeyCode.Return))
-        {
-            Player_Mode = 10;
-            Debug.Log("キャラ選択終わり");
-        }
-    }
-
-    //機体番号の受け取りと切り替え
-    private void KitaiChange()
-    {
-        switch (MachineNum)
-        {
-            case 0:
-                //次のオブジェクトをアクティブ化
-                //kitaiObject[MachineNum].SetActive(true);
-
-                Skin.transform.GetChild(MachineNum).gameObject.SetActive(true);
-                Debug.Log("機体番号 : " + MachineNum);
-                break;
-            case 1:
-                //次のオブジェクトをアクティブ化
-                //kitaiObject[MachineNum].SetActive(true);
-
-                Skin.transform.GetChild(MachineNum).gameObject.SetActive(true);
-                Debug.Log("機体番号 : " + MachineNum);
-                break;
-            case 2:
-                //次のオブジェクトをアクティブ化
-                //kitaiObject[MachineNum].SetActive(true);
-
-                Skin.transform.GetChild(MachineNum).gameObject.SetActive(true);
-                Debug.Log("機体番号 : " + MachineNum);
-                break;
-            case 3:
-                break;
-            default:
-                Debug.LogError("機体番号が呼び出されませんでした");
-                break;
+            Player_Mode = 20;
+            Debug.Log("レース中");
         }
     }
 }

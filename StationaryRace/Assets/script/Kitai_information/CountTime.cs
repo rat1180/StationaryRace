ea9b628dc0468195159f639b587f1ae33b26061f
siitakee <r03.gm.2110016@da.iwasaki.ac.jp>
2022-06-13 15:34:27 +0900

    a

---
+++ StationaryRace/Assets/script/Kitai_information/CountTime.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountTime : MonoBehaviour
+{
+    public static CountTime instance;
+
+    public float cnttime = 3; 
+    public GameObject CountText;   // テキストの表示用
+    public GameObject panel; // パネルの表示切替
+    public bool cntflg;
+    public int gamestart = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cntflg = true;
+        gamestart = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetCountTime();
+
+        // Debug.Log("gamestart = " + gamestart);
+    }
+
+    /// <summary>
+    /// カウントの表示
+    /// </summary>
+    public int GetCountTime()
+    {
+        if (cntflg == true)
+        {
+            cnttime -= Time.deltaTime; // カウントダウン
+
+            if (cnttime >= 0.5f)
+            {
+                CountText.GetComponent<Text>().text = "" + cnttime.ToString("f0");
+            }
+            else if (cnttime <= 0.5f)
+            {
+                CountText.GetComponent<Text>().text = "GO";
+                gamestart = 1;
+                //Debug.Log("GO");
+            }
+            if (cnttime <= -1)
+            {
+                panel.SetActive(false);
+                cntflg = false;
+            }
+        }
+        return gamestart;
+    }
+
+    public int GetGameStart()
+    {
+        return gamestart;
+    }
+
+}

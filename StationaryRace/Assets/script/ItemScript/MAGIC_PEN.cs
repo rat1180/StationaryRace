0aabe8f87b1d82482b4818f9493cb4481c736252
HirotoNagatsuka <93108443+HirotoNagatsuka@users.noreply.github.com>
2022-06-27 11:54:56 +0900

    ham

--- StationaryRace/Assets/script/ItemScript/MAGIC_PEN.cs
+++ StationaryRace/Assets/script/ItemScript/MAGIC_PEN.cs
@@ -10,26 +10,20 @@ public class MAGIC_PEN : MonoBehaviour
     public int durability;
     GameObject ItemMana;
     ItemManager IMana;
-    GameObject ItemUI;
-    GameObject INDIA_INK;
-    INDIA_INK InkSc;
 
     // Start is called before the first frame update
     void Start()
     {
         ItemMana = GameObject.Find("ITEMManager");  //アイテムマネージャーを取得.
         IMana = ItemMana.GetComponent<ItemManager>();
-        ItemUI = transform.GetChild(0).gameObject;
-        INDIA_INK = ItemUI.GetComponent<Transform>().transform.GetChild(0).gameObject;
-
-        InkSc = INDIA_INK.GetComponent<INDIA_INK>();
 
         rb = GetComponent<Rigidbody>();
-        speed = 8.0f;
+        speed = 40.0f;
         //Invoke("Des", 10);
         rb.velocity = transform.forward * speed;
 
         durability = 1;
+        
     }
 
     // Update is called once per frame
@@ -52,15 +46,12 @@ public class MAGIC_PEN : MonoBehaviour
 
     void OnTriggerEnter(Collider collider)
     {
-        InkSc.Animation();
         durability -= 1;
         // 衝突した相手にPlayerタグが付いているとき.
         if (collider.gameObject.tag == "Player")
         {
-            Debug.Log("hitoooooooo");
-            //InkSc.Animation();
-            //durability -= 1;
 
         }
     }
 }
+

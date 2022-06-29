9b47162811b0a9295ff610ce6d2f9107496502b5
rat1180 <r03.gm.2110112@da.iwasaki.ac.jp>
2022-06-29 12:03:22 +0900

    a

--- StationaryRace/Assets/script/ItemScript/MECHANICAL_PEN_LEAD.cs
+++ StationaryRace/Assets/script/ItemScript/MECHANICAL_PEN_LEAD.cs
@@ -7,23 +7,31 @@ public class MECHANICAL_PEN_LEAD : MonoBehaviour
 
     private Rigidbody rb;
     private float speed;
-    private int durability;
+    public int durability;
     Quaternion kai;
+    GameObject Player;         //プレイヤーのゲームオブジェクトを取得する準備.
+    Car CarSc;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        speed = 5.0f;
-        Invoke("Des", 3);
+        speed = 40.0f;
+        Invoke("Des", 20);
         rb.velocity = transform.forward * speed;
+        durability = 1;
+        Player = GameObject.Find("Car");         //プレイヤーのゲームオブジェクトを取得.
+        CarSc = Player.GetComponent<Car>(); //プレイヤーのスクリプトを参照する.
     }
 
     // Update is called once per frame
     void Update()
     {
         rb.velocity = transform.forward * speed;
-        //transform.Rotate(90, 0, 0);
+        if (durability == 0)
+        {
+            Des();
+        }
     }
 
 
@@ -33,12 +41,30 @@ public class MECHANICAL_PEN_LEAD : MonoBehaviour
         Destroy(this.gameObject);
     }
 
+    //ステージに当たった際にフラグを切り替える
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            {
+                durability -= 1;
+                CarSc.SpeedDown();
+            }
+        }
+    }
+
     void OnTriggerEnter(Collider collider)
     {
+        if (collider.gameObject.tag == "Stage")
+        {
+            rb.isKinematic = true;
+            this.GetComponent<Rigidbody>().useGravity = false;//グラビティをなくす
+        }
         // 衝突した相手にPlayerタグが付いているとき.
         if (collider.gameObject.tag == "Player")
         {
             durability -= 1;
+            CarSc.SpeedDown();
         }
     }
 }
